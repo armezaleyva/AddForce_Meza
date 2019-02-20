fc1d1479ec668020840c632dc98d6d4ad4acd6eb
Armando Meza Leyva <armezaleyva@gmail.com>
2019-02-20 08:34:16 -0700

    Caja con addforce

---
+++ Assets/Scripts/BoxPhysics.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxPhysics : MonoBehaviour
+{
+    Rigidbody2D rigidBody2D;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+        rigidBody2D.AddForce(Vector2.up * 500, ForceMode2D.Force);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
